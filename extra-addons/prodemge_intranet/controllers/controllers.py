c97463d1aaa5e339f804758a53ec9873e7e59a56
root <root@Triton300SE>
2024-05-03 11:30:16 -0300

    Inclusão de validação de grupo de acesso nas views e controllers.

--- extra-addons/prodemge_intranet/controllers/controllers.py
+++ extra-addons/prodemge_intranet/controllers/controllers.py
@@ -6,5 +6,7 @@ class IntranetProdemgeController(http.Controller):
 
     @http.route('/intranet', type='http', auth='user')
     def intranet_prodemge(self, **kwargs):
+        if not request.env.user.has_group('prodemge_intranet.intranet_user'):
+            return "Você não tem permissão para acessar esta página."
         intranet_prodemge_url = request.env['ir.config_parameter'].sudo().get_param('prodemge_intranet.intranet_prodemge_url')
         return request.render('prodemge_intranet.intranet_iframe', {'intranet_prodemge_url': intranet_prodemge_url})
--- extra-addons/prodemge_painelnegocios/controllers/controllers.py
+++ extra-addons/prodemge_painelnegocios/controllers/controllers.py
@@ -6,5 +6,7 @@ class PainelNegociosController(http.Controller):
 
     @http.route('/painel_negocios', type='http', auth='user')
     def painel_negocios(self, **kwargs):
+        if not request.env.user.has_group('prodemge_painelnegocios.painelnegocios_user'):
+            return "Você não tem permissão para acessar esta página."
         painel_negocios_url = request.env['ir.config_parameter'].sudo().get_param('prodemge_painelnegocios.painel_negocios_url')
         return request.render('prodemge_painelnegocios.painelnegocios_iframe', {'painel_negocios_url': painel_negocios_url})
